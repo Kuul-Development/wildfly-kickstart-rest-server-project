239fd40785527b72cae16a872150e33eddb90c2b
petermkr <mail@peter-kroiss.de>
2020-05-25 01:18:08 +0200

    Add basic configuration for Rest endpoints

---
+++ src/main/java/com/kerkro/tech/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.kerkro.tech;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class ApplicationConfig extends Application {
+}
